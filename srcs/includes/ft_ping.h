418890a1db8b1e2e786bf3ba07a06e9ae004a8ce
emenella <emenella@student.42lyon.fr>
2024-01-12 17:32:09 +0100

    function declaration added

--- srcs/includes/ft_ping.h
+++ srcs/includes/ft_ping.h
@@ -14,7 +14,28 @@
 
 #define PING_MAXWAIT 10
 #define PING_MAXPACKET 4096
+#define PING_HOST_MAXLEN 64
 #define PING_VERBOSE 1
 #define PING_RECORD 2
 #define PING_DEBUG 4
 #define PING_QUIET 8
+
+u_char  packet[PING_MAXPACKET];
+int     pingFlags;
+int     socket;
+hostent *hp;
+timezone tz;
+int      datalen;
+char usage[] = "Todo";
+char *hostname;
+char hostbuffer[PING_HOST_MAXLEN];
+
+int nreceived = 0;
+int timing = 0;
+int tmin = 999999999;
+int tmax = 0;
+int t_sum = 0;
+
+int     finish(), catcher(), pinger();
+
+void    pr_pack(char *buf, void *cc, void *from);
